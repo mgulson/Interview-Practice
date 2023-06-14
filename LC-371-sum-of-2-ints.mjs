c10acf5e27881c992fac4dd7e7ca9b0c0529d977
Michael Gulson <mgulson@optoro.com>
2023-06-13 21:43:30 -0400

    updates for lc-371

--- LC-371-sum-of-2-ints.mjs
+++ LC-371-sum-of-2-ints.mjs
@@ -21,11 +21,50 @@ class binarySum {
     return num.toString(2)
   }
 
+  addBinaryNumbersOne(a, b, carryOver){
+    //strings
+    let result;
+    if(a === '1' && b === '1' && carryOver === 1) {
+      result = 1
+      carryOver = 1
+    } else if(((a === '1' || b === '1') && carryOver === 1) || a === '1' && b === '1'){
+      result = 0
+      carryOver = 1
+    } else if( a === '1' || b === '1' || carryOver === 1) {
+      result = 1
+      carryOver = 0
+    } else {
+      result = 0
+      carryOver = 0
+    }
+
+    return { result, carryOver}
+  }
+
+  calculateRemaining(diff, originalCarryOver) {
+    let length = diff.length
+    let returnValue = ''
+    let carryOver = originalCarryOver
+    for(let i  = 0; i< length + 1; i++){
+      let a = diff[length - i - 1]
+
+      const { result, carryOver: myCarryOver } = this.addBinaryNumbersOne(a, carryOver.toString(), 0)
+
+      carryOver = myCarryOver
+      returnValue = result + returnValue    
+    }
+
+    return returnValue
+  }
+
   addBinary(num1, num2){
     let sum = ""
     let carryOver = 0
     const length1 = num1.length
     const length2 = num2.length
+    const myThis = this
+    const arr = [1,2,3]
+    arr.forEach((element) => console.log(myThis))
 
     for(let i=0; i < Math.max(length1, length2)+ 1; i++){
       if( length1 != length2 && i > Math.min(length1 , length2) - 1){
@@ -36,7 +75,8 @@ class binarySum {
           diff = num2.slice(0, length2 - length1)
         }
 
-        sum = diff + sum
+        const remaining = this.calculateRemaining(diff, carryOver)
+        sum = remaining + sum
         break
       } else if(i > Math.min(length1, length2) -1) {
         //implement mismatches
@@ -47,13 +87,13 @@ class binarySum {
         let b = num2[length2 - i - 1]
         let c = 0
         
-        if(a === '1' && b === '1' && carryOver === '1') {
+        if(a === '1' && b === '1' && carryOver === 1) {
           c = 1
           carryOver = 1
-        } else if(((a === '1' || b === '1') && carryOver === '1') || a === '1' && b === '1'){
+        } else if(((a === '1' || b === '1') && carryOver === 1) || a === '1' && b === '1'){
           c = 0
           carryOver = 1
-        } else if( a === '1' || b === '1' || carryOver === '1') {
+        } else if( a === '1' || b === '1' || carryOver === 1) {
           c = 1
           carryOver = 0
         } else {
@@ -76,4 +116,8 @@ console.log('2 + 2 = 100 :', bs.binary)
 bs = new binarySum(10, 5)
 
 console.log('10 + 5 =  1111 : ', bs.binary)
+
+bs = new binarySum(20, 5)
+
+console.log('20 + 5 = 011001: ', bs.binary)
 
