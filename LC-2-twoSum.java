17143d58d6a403783beb45300b236a841e8b3e47
Michael Gulson <msgulson@gmail.com>
2023-11-07 14:16:29 -0600

    java LC 2

--- LC-2-twoSum.java
+++ LC-2-twoSum.java
@@ -1,6 +1,16 @@
 import java.util.HashMap;
+import java.util.Arrays;
 
 class Solution {
+
+    public static void main(String[] args){
+      int[] arr = {2,7,11,15};
+      int target = 9;
+
+      Solution solution = new Solution();
+      System.out.println(Arrays.toString(solution.twoSum(arr, target)));
+    }
+
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> complements = new HashMap<>();
         
